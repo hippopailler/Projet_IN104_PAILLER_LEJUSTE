f68e4c5159a9fdda7f997334e15c5c47a1e0b855
hippopailler <hippolyte.pailler@ensta-paris.fr>
2024-04-14 08:40:09 +0200

    Création de la fonction victoire pour la fin de la partie et ajout dans le makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 CFLAGS = -Wall -Werror
 
-SOURCES = affichage.c position_bobai.c position_pion.c main.c
+SOURCES = affichage.c position_bobai.c position_pion.c victoire.c main.c
 OBJS = $(SOURCES:.c=.o)
 
 EXECUTABLE = main.x
--- victoire.c
+++ victoire.c
@@ -5,7 +5,9 @@
 #include "case.h"
 #include <stdbool.h>
 
-bool position_pion (CouleurPion** grille, CouleurPion joueur){
+bool victoire (CouleurPion** grille, CouleurPion joueur){
+    // passage de la variable victory en true pour marqué une fin de partie
+    // récupération position bobail
     int ligne;
     int colonne;
     for (int i=0; i<5;++i){
@@ -13,11 +15,13 @@ bool position_pion (CouleurPion** grille, CouleurPion joueur){
             if (grille[i][j]==VERT){
                 if (joueur ==BLEU){
                     if (i==0){
-                        return false;
+                        printf("bord");
+                        return true;
                     }
                 }
                 if (joueur==ROUGE){
-                    if (i==5){
+                    if (i==4){
+                        printf("bord");
                         return true;
                     }
                 }
@@ -26,5 +30,22 @@ bool position_pion (CouleurPion** grille, CouleurPion joueur){
             }
         }
     }
+    // le bobail peut-il encore bouger ? existence d'une case blanche autour de lui ?
+
+    for (int i=-1;i<=1;++i){
+        for (int j=-1;j<=1;++j){
+            //on vérifie les sorties de terrain
+            if (0<=ligne+i && ligne+i<=4){
+                if (0<=colonne+j && colonne+j<=4){
+                    if (grille[ligne+i][colonne+j]==BLANC){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    //aucune case blanche autour du bobail
+    printf("calins");
+    return true;
 }
 
