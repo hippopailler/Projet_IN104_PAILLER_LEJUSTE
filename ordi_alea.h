98340cfe2c67e7027b56949fca8d449e8eac4773
Aliona Lejuste <alejuste2@hotmail.com>
2024-04-23 17:15:02 +0200

    fonction a corirger + header

---
+++ ordi_alea.h
@@ -0,0 +1,11 @@
+#ifndef ORDI_ALEA_H
+#define ORDI_ALEA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "case.h"
+
+CouleurPion** position_bobai_alea (CouleurPion** grille);
+CouleurPion** position_pion_alea (CouleurPion** grille);
+
+#endif /* ORDI_ALEA_H */
