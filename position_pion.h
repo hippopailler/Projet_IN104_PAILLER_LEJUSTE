08c4ae4db70d61ce751ba18fdc55786e001487d0
Aliona Lejuste <alejuste2@hotmail.com>
2024-04-23 15:32:13 +0200

    header positon

---
+++ position_pion.h
@@ -0,0 +1,10 @@
+#ifndef POSITION_PION_H
+#define POSITION_PION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "case.h"
+
+CouleurPion** position_pion(CouleurPion** grille, CouleurPion joueur);
+
+#endif /* POSITION_PION_H */
